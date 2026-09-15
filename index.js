109d6b6c81023e366bc8f3a9f75d4926fd8b4631
sabujAlom <sabujalom18@gmail.com>
2026-09-15 12:40:53 +0600

    completed initial setup

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT
+const { MongoClient }= require('mongodb');
+
+
+const client = new MongoClient(process.env.DB_URI);
+
+ async function connectToMongoDB() {
+  try {
+    await client.connect();
+
+
+    console.log("You successfully connected to MongoDB!");
+    return client;
+  } catch (err) {
+    console.dir(err);
+  }
+}
+connectToMongoDB();
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
